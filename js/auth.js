7e89be0e54ad1e20c42ec72dfabafa40ac016a42
Brayanklein555 <brayanprogramacao@gmail.com>
2026-03-07 01:35:02 -0300

    Change login credentials from 'admin' to 'cliente'

--- js/auth.js
+++ js/auth.js
@@ -3,9 +3,7 @@ function login(){
 let user=document.getElementById("user").value
 let pass=document.getElementById("pass").value
 
-if(user=="admin" && pass=="1234"){
-
-localStorage.setItem("login","true")
+if(user=="cliente" && pass=="1234"){
 
 window.location="dashboard.html"
 
