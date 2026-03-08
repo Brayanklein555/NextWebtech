732bf62bc0d2b536f6122e9e11402df0631c622f
Brayanklein555 <brayanprogramacao@gmail.com>
2026-03-08 00:08:14 -0300

    Update porfolio.js

--- js/porfolio.js
+++ js/porfolio.js
@@ -1,18 +1,18 @@
 fetch("data/projects.json")
 
-.then(res=>res.json())
+.then(r=>r.json())
 
 .then(data=>{
 
-const container=document.getElementById("portfolio")
+let container=document.getElementById("portfolio")
 
-data.forEach(project=>{
+data.forEach(p=>{
 
-const div=document.createElement("div")
+let div=document.createElement("div")
 
 div.className="project"
 
-div.innerHTML=`<img src="${project.imagem}">`
+div.innerHTML=`<img src="${p.imagem}">`
 
 container.appendChild(div)
 
