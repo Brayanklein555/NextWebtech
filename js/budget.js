5815b8da0564eee714c73f26f78e3a42fa22aa91
Brayanklein555 <brayanprogramacao@gmail.com>
2026-03-08 00:11:17 -0300

    Update budget.js

--- js/budget.js
+++ js/budget.js
@@ -1,4 +1,4 @@
-function gerarOrcamento(){
+function gerar(){
 
 let tipo=document.getElementById("tipo").value
 
@@ -6,7 +6,8 @@ let preco=0
 
 if(tipo=="site")preco=1500
 if(tipo=="loja")preco=3000
+if(tipo=="design")preco=500
 
-document.getElementById("resultado").innerText="R$ "+preco
+document.getElementById("valor").innerText="R$ "+preco
 
 }
